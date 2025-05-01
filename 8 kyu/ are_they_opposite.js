02ddd3ee348a18da2fbcb07780aec88cd377b164
Viktor Kuznetsov <albatros90albatros90@gmail.com>
2025-05-01 07:30:52 +0300

    Fix problem (solved)

--- 8 kyu/ are_they_opposite.js
+++ 8 kyu/ are_they_opposite.js
@@ -1,4 +1,12 @@
 function isOpposite(s1,s2){
-    if(s1.lenght !== s2.lenght || s1 === "" || s2 == "")
+    if (s1.toLowerCase() !== s2.toLowerCase() || s1 === s2)
         return false;
+    for (let i = 0; i < s1.length; i++) {
+        if (s1.charAt(i) === s2.charAt(i)) return false
+    }
+    return true;
 }
+
+let s1 = "AB"
+let s2 = "ab"
+console.log(isOpposite(s1, s2));
